10acddb14411fc118e7fa79194507d8ca9203937
Owen Willis <owenwillis@gmail.com>
2016-04-29 14:22:30 -0400

    twitter pagination

--- powwows/views.py
+++ powwows/views.py
@@ -3,17 +3,22 @@ from django.shortcuts import render, get_object_or_404
 
 from django.utils import timezone
 from .models import Post
+from el_pagination.decorators import page_template
 
-
+@page_template('powwows/post_list_page.html')
 # Create your views here.
-def post_list(request, *args, **kwargs):
+def post_list(request, template = 'powwows/post_list.html', extra_context = None, *args, **kwargs):
 
     messages.success(request, 'Welcome to powwow! We collect comments from news stories and share them in a single feed. Enjoy!')
     
-
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    context={
+    'posts':Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     
-    return render(request, 'powwows/post_list.html', {'posts':posts})
+    }
+    if extra_context is not None:
+    	context.update(extra_context)
+    
+    return render(request, template, context)
 
 
 
